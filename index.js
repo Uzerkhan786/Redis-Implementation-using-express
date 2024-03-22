cd56d36adf6159cbeadb934b2ba8214a7236759c
Uzerkhan786 <uzer.khan2020@vitbhopal.ac.in>
2024-03-22 06:32:42 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const Redis = require('ioredis');
+const redis = new Redis();
+
+const app = express();
+
+
+app.get('/', async (req, res) => {
+    const a = await redis.get('api');
+    if (a) {
+        return res.json({
+            data: JSON.parse(a)
+        })
+    }
+    const ab = await fetch('https://toursbooking-6nzn.onrender.com/api/v1/tours');
+    const json = await ab.json();
+    await redis.set('api', JSON.stringify(json))
+    res.json({
+        data: json
+    })
+})
+
+app.listen(3000, () => {
+    console.log('server is up on 3000')
+})
